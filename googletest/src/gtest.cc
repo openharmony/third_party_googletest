4f0298a0756f13cbe08dced695255cdefe43a6ae
txl <tangxianglei@huawei.com>
2024-01-26 16:45:35 +0800

    googletest_1.13.0 tc.size参数
    Signed-off-by: txl <tangxianglei@huawei.com>

--- googletest/src/gtest.cc
+++ googletest/src/gtest.cc
@@ -6593,8 +6593,7 @@ static bool ParseGoogleTestFlag(const char* const arg) {
   GTEST_INTERNAL_PARSE_FLAG(stack_trace_depth);
   GTEST_INTERNAL_PARSE_FLAG(stream_result_to);
   GTEST_INTERNAL_PARSE_FLAG(throw_on_failure);
-  ParseFilterFlag(arg);
-  return false;
+  return ParseFilterFlag(arg);
 }
 
 #if GTEST_USE_OWN_FLAGFILE_FLAG_ && GTEST_HAS_FILE_SYSTEM
